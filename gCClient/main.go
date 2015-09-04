4de96bc732094525e372075651190b6ed067b6c3
Anmol Sethi <anmol@aubble.com>
2015-09-04 13:52:39 -0400

    redone client and fixed bs

--- gCClient/main.go
+++ gCClient/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -13,78 +12,62 @@ import (
 )
 
 func main() {
-	log.SetPrefix("client: ")
-	tmp := flag.String("addr", ":5000", "listen address")
-	flag.Parse()
-	osr := bufio.NewReader(os.Stdin)
-	addr := *tmp
-	for ; ; addr = "" {
-		for addr == "" {
-			var err error
-			fmt.Print("enter address (host:port): ")
-			addr, err = osr.ReadString('\n')
+	log.SetPrefix("gCClient: ")
+	if len(os.Args) < 2 {
+		log.Print("gCClient host:port")
+		return
+	}
+	addr := os.Args[1]
+	if !strings.Contains(addr, ":") {
+		addr = ":" + addr
+	}
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	var userEnteredMutex sync.Mutex
+	var userEntered bool
+	go func() {
+		defer c.Close()
+		osr := bufio.NewReader(os.Stdin)
+		for {
+			m, err := osr.ReadString('\n')
 			if err != nil {
-				log.Print(err)
-				continue
+				log.Print('\n', err)
+				return
 			}
-			addr = addr[:len(addr)-1]
+			userEnteredMutex.Lock()
+			userEntered = true
+			userEnteredMutex.Unlock()
+			_, err = fmt.Fprintf(c, "%s", m)
+			if err != nil {
+				log.Print('\n', err)
+				return
+			}
 		}
-		if !strings.Contains(addr, ":") {
-			addr = ":" + addr
-		}
-		c, err := net.Dial("tcp", addr)
+	}()
+	cr := bufio.NewReader(c)
+	newLine := true
+	for {
+		r, _, err := cr.ReadRune()
 		if err != nil {
-			log.Print(err)
-			continue
+			fmt.Fprint(os.Stderr, "\n")
+			log.Println('\n', err)
+			return
 		}
-		var userEnteredMutex sync.Mutex
-		var userEntered bool
-		var wg sync.WaitGroup
-		wg.Add(2)
-		go func() {
-			defer wg.Done()
-			for {
-				m, err := osr.ReadString('\n')
-				if err != nil {
-					log.Print('\n', err)
-					return
-				}
-				userEnteredMutex.Lock()
-				userEntered = true
-				userEnteredMutex.Unlock()
-				_, err = fmt.Fprintf(c, "%s", m)
-				if err != nil {
-					log.Print('\n', err)
-					return
-				}
-			}
-		}()
-		go func() {
-			defer c.Close()
-			defer wg.Done()
-			cr := bufio.NewReader(c)
-			newLine := true
-			for {
-				r, _, err := cr.ReadRune()
-				if err != nil {
-					log.Print('\n', err)
-					return
-				}
-				userEnteredMutex.Lock()
-				if newLine == true || userEntered == true {
-					fmt.Print(time.Now().Format("15:04 "))
-					newLine = false
-					userEntered = false
-				}
-				userEnteredMutex.Unlock()
-				if r == '\n' {
-					newLine = true
-				} else {
-					newLine = false
-				}
-				fmt.Print(string(r))
-			}
-		}()
-		wg.Wait()
+		userEnteredMutex.Lock()
+		if newLine == true || userEntered == true {
+			fmt.Print(time.Now().Format("15:04 "))
+			newLine = false
+			userEntered = false
+		}
+		userEnteredMutex.Unlock()
+		if r == '\n' {
+			newLine = true
+		} else {
+			newLine = false
+		}
+		fmt.Print(string(r))
 	}
 }
